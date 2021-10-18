fd69ae2e99d232c22bf9b3a5576ca0bd65756605
Timothy Rich <TimothyR2@reutech.co.za>
2021-10-18 15:10:53 +0200

    Update

--- components/Main.js
+++ components/Main.js
@@ -13,35 +13,33 @@ function Main() {
     return (
 
 
-        <div>
+        <div className="relative ">
             
-            <div className="flex items-center md:border-2 rounded-full py-2
-                            md:shadow-sm">
-                <input
-                    value={idNumber}
-                    onChange={(e) => setidNumber(e.target.value)}
-                    className="w-96 sm:w-150 pl-5 bg-transparent outline-none flex-grow text-m
-                    text-gray-600 placeholder-gray-400" 
-                    type="text" placeholder="Please enter a RSA ID number"
-                />
-                <FingerPrintIcon className="h-8 pr-2 py-1 cursor-pointer"
-                            onClick={Validator}
-                />
+            <div className="">
+                <div className="flex items-center md:border-2 rounded-full py-2
+                                md:shadow-sm">
+                    <input
+                        value={idNumber}
+                        onChange={(e) => setidNumber(e.target.value)}
+                        className="w-96 pl-5 bg-transparent outline-none flex-grow text-m
+                        text-gray-600 placeholder-gray-400" 
+                        type="text" placeholder="Please enter a RSA ID number"
+                    />
+                    <FingerPrintIcon className="h-8 pr-2 py-1 cursor-pointer"/>
+                </div>
             </div>
-            <div >
+            
+            <div className="absolute">
                 <button className="flex text-white bg-green-600 px-6
                                     py-4 rounded-full shadow-md
                                     font-bold my-3 hover:shadow-xl
                                     active:scale-90 transition duration-150"
                         onClick={Validator(idNumber)}>
-                    Check ID <CheckIcon className="h-5 my-auto pl-2"/>
+                    Check ID 
+                    <CheckIcon className="h-5 my-auto pl-2"/>
                 </button>
             </div>
             
-            <script>
-                document.getElementById("result").innerHTML = "Valid RSA ID number"
-            </script>
-            <h2 id="result"></h2>
         </div>
     )
 }
